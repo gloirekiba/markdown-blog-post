976be6e09b62d794ee72d0a390174b5827b152d9
gloirekiba <gloirekiba21@gmail.com>
2022-08-16 00:43:45 +0200

    ✨ add database connection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const articlesRouter = require("./routes/articles.route");
 
@@ -15,8 +16,16 @@ app.use(express.json());
 
 app.use("/articles", articlesRouter);
 
-const PORT = process.env.PORT;
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
