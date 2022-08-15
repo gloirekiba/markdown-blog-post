3309a076dc764261283379c66f3532712ad5d44d
gloirekiba <gloirekiba21@gmail.com>
2022-08-15 23:18:19 +0200

    🎉 initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
