30945d3e7b25a44474bbe9a7bdab5cefa4c5c7d5
gloirekiba <gloirekiba21@gmail.com>
2022-08-16 00:11:14 +0200

    ✨ add index route

---
+++ routes/articles.route.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const articles = [
+    {
+      title: "Article 1",
+      createdAt: new Date(),
+      content:
+        "This is the content of article 1 and it is very interesting indeed.",
+    },
+    {
+      title: "Article 2",
+      createdAt: new Date(),
+      content:
+        "This is the content of article 2 and it is very interesting indeed.",
+    },
+  ];
+  res.render("articles/index", { articles });
+});
+
+module.exports = router;
