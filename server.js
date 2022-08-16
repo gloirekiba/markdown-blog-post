103b29863d455f1224c25b828c91dd5527df0bd9
gloirekiba <gloirekiba21@gmail.com>
2022-08-16 12:28:21 +0200

    ✨ add new middleware

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/articles", articlesRouter);
