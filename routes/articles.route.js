9fbf403139035d87223018178069a00b30b9ae89
gloirekiba <gloirekiba21@gmail.com>
2022-08-16 15:59:07 +0200

    ♻️ refactor articles route

--- routes/articles.route.js
+++ routes/articles.route.js
@@ -10,7 +10,8 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/new", (req, res) => {
-  res.render("articles/new");
+  const article = new Article();
+  res.render("articles/new", { article });
 });
 
 router.get("/edit/:id", async (req, res) => {
@@ -18,26 +19,23 @@ router.get("/edit/:id", async (req, res) => {
   res.render("articles/edit", { article });
 });
 
-router.post("/", async (req, res) => {
-  const { title, description, markdown } = req.body;
-  try {
-    const newArticle = new Article({
-      title,
-      description,
-      markdown,
-    });
-    await newArticle.save();
-    res.redirect(`articles/${newArticle.slug}`);
-  } catch (error) {
-    console.log(error);
-    res.render("articles/new", {
-      alert: {
-        type: "danger",
-        message: error.message,
-      },
-    });
-  }
-});
+router.post(
+  "/",
+  async (req, res, next) => {
+    req.article = new Article();
+    next();
+  },
+  saveArticleAndRedirect("new")
+);
+
+router.put(
+  "/:id",
+  async (req, res, next) => {
+    req.article = await Article.findById(req.params.id);
+    next();
+  },
+  saveArticleAndRedirect("edit")
+);
 
 router.delete("/:id", async (req, res) => {
   await Article.findByIdAndDelete(req.params.id);
@@ -49,5 +47,20 @@ router.get("/:slug", async (req, res) => {
   if (article == null) res.redirect("/");
   res.render("articles/show", { article });
 });
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article;
+    article.title = req.body.title;
+    article.description = req.body.description;
+    article.markdown = req.body.markdown;
+    try {
+      article = await article.save();
+      res.redirect(`/articles/${article.slug}`);
+    } catch (error) {
+      res.render(`articles/${path}`, { article });
+    }
+  };
+}
 
 module.exports = router;
